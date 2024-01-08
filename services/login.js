48ae5fcd197257b604b76b0ede50b3a3c4cec15a
Manohar Ramarao <manoharramarao@gmail.com>
2024-01-08 18:40:12 +0530

    created jwt token on successful login

--- services/login.js
+++ services/login.js
@@ -1,40 +1,43 @@
-
 const db = require("../models");
-var createError = require('http-errors')
+var createError = require("http-errors");
 const { Op } = require("sequelize");
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const encryptPassword = async (password, salt) => {
   var hash = bcrypt.hashSync(password, salt);
   return hash;
-}
+};
 
 const comparePasswords = async (password, passwordFromDB) => {
-  return (password === passwordFromDB) ? true : false;
-}
+  return password === passwordFromDB ? true : false;
+};
 
 module.exports = async function (email, password) {
   console.log(`Inside login service`);
   console.log(`Please execute login for user ${email} and pass ${password}`);
   try {
     let dbUser = await db.users.findAll({
-    where: {
-      email: {
-        [Op.eq]: email
-      }
-    }
+      where: {
+        email: {
+          [Op.eq]: email,
+        },
+      },
     });
     dbUser = dbUser[0].dataValues;
     const passwordFromUI = await encryptPassword(password, dbUser.salt);
-    const result =  await comparePasswords(passwordFromUI, dbUser.password)
+    const result = await comparePasswords(passwordFromUI, dbUser.password);
     if (result) {
-      console.log(`generate Token`);
+      const token = jwt.sign({ userId: dbUser.email }, "your-secret-key", {
+        expiresIn: "1h",
+      });
+      return { token: token };
     } else {
       console.log(`userID password didn't match`);
     }
   } catch (error) {
     console.error(`error is`);
     console.error(error);
-    throw createError(500, 'Something went wrong!');
+    throw createError(500, "Something went wrong!");
   }
-}
+};
